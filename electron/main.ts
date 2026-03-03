365ed2a7aac8f78e2f2f9c57d4f1179a4e615927
Felix-au <harshit.soni.23cse@bmu.edu.in>
2026-03-03 11:02:41 +0000

    fix: Resolve memory leak in electron main process

--- electron/main.ts
+++ electron/main.ts
@@ -1,2 +1,6 @@
 import { app, BrowserWindow } from 'electron';
-console.log('Starting CorvusX Main Process...');
+let win: BrowserWindow | null = null;
+app.whenReady().then(() => {
+  win = new BrowserWindow();
+  win.on('closed', () => { win = null; });
+});
